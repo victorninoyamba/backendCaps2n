b9da5ea2a459e94c69f498889cf47fd658beec48
victorninoyamba <victorninoyamba@gmail.com>
2023-06-27 22:57:33 +0800

    addProperties.js and sampleSending,js

--- client/src/components/addProperties.js
+++ client/src/components/addProperties.js
@@ -3,8 +3,97 @@ import AdminHome from "./adminHome";
 export default function addProperties() {
   return (
     <>
-      <AdminHome />
-      <h1>Add Properties</h1>
+     {/* <AdminHome /> */}
+      <div className=" flex justify-center pt-5 pb-5  ">
+    <div class="w-1/2  p-6 bg-white border border-gold rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <h1 className="flex justify-center text-lg">Property Information</h1>
+    <form className="flex-wrap w-full">
+        <div className="grid gap-6 mb-6 md:grid-cols-2">
+        </div>
+        
+        <label for="PropertyType" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Property Type</label>
+        <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option selected>Choose Type</option>
+            <option value="PS">Preselling</option>
+            <option value="RFO">Ready For Occupancy</option>
+        </select>
+
+        <div>
+            <label for="first_name" className="block mb-1 pt-2 text-sm font-medium text-gray-900 dark:text-white">Selling Price</label>
+            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+        </div>
+        <div>
+            <label for="last_name" className="block mb-1 pt-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+            <input type="text" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+        </div>
+        <div>
+            <label for="last_name" className="block mb-1 pt-2 text-sm font-medium text-gray-900 dark:text-white">Bedrooms</label>
+            <input type="text" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required/>
+        </div>
+        <div>
+            <label for="last_name" className="block mb-1 pt-2 text-sm font-medium text-gray-900 dark:text-white">Bathrooms</label>
+            <input type="text" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required/>
+        </div>
+        <div>
+            <label for="company" className="block mb-1 pt-2 text-sm font-medium text-gray-900 dark:text-white">Car Parks</label>
+            <input type="text" id="company" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+        </div>  
+        <div>
+            <label for="company" className="block mb-1 pt-2 text-sm font-medium text-gray-900 dark:text-white">Floor Area</label>
+            <input type="text" id="company" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+        </div>  
+        <div>
+            <label for="message" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Home Features</label>
+            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=" Please separate each features using a comma"></textarea>
+        </div>
+        <div>
+            <label for="message" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Neighborhood Features</label>
+            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=" Please separate each features using a comma"></textarea>
+        </div>
+        <h2 className="flex pt-3 text-sm ">Nearby Establishments</h2>
+        <div>
+            <label for="message" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Food Hubs</label>
+            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=" Please separate each features using a comma"></textarea>
+            <label for="message" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Grocery</label>
+            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=" Please separate each features using a comma"></textarea>
+            <label for="message" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Gym</label>
+            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=" Please separate each features using a comma"></textarea>
+            <label for="message" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">School</label>
+            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=" Please separate each features using a comma"></textarea>
+            <label for="message" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Mall/Store</label>
+            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=" Please separate each features using a comma"></textarea>
+
+            <label for="message" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Hospital</label>
+            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=" Please separate each features using a comma"></textarea>
+            
+        </div>
+        <div>
+            <label for="message" class="block pt-3 mb-1 text-sm font-medium text-gray-900 dark:text-white">Neighborhood Vicinity</label>
+            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=" Please separate each features using a comma"></textarea>
+            
+        </div>
+
+        <div class="pt-4 pb-3 flex items-center justify-center w-full">
+            <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+             <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                 <p class="mb-1 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                 <p class="text-xs text-gray-500 dark:text-gray-400">You can upload up to 5 images.</p>
+             </div>
+             <input id="dropzone-file" type="file" class="hidden" />
+             </label>
+        </div> 
+
+        <div className="flex items-start mb-6">
+        <div className="flex items-center h-5">
+        <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required/>
+        </div>
+        <label for="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">Terms and Conditions</a>.</label>
+    </div>
+    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Upload</button>
+</form>
+</div>
+</div>
     </>
   );
 }
